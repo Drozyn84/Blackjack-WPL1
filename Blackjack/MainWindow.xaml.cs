17ab4c3c394289b9b11ee3eb2c37eba53a3880ad
Drozyn84 <114404360+Drozyn84@users.noreply.github.com>
2022-11-21 22:08:09 +0100

    trying randomizer

--- Blackjack/MainWindow.xaml.cs
+++ Blackjack/MainWindow.xaml.cs
@@ -20,38 +20,100 @@ namespace Blackjack
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Random random = new Random();
+        private Random random1 = new Random();
+        private StringBuilder sb;
+        private StringBuilder sb1;
+        private int willekeurigeKaart;
+        private int willekeurigeKaartBank;
+        private int volgBank;
+        private int volgSpel;
         public MainWindow()
         {
-            
+
             InitializeComponent();
 
             btnHit.IsEnabled = false;
             btnStand.IsEnabled = false;
-         
+
+            sb = new StringBuilder();
+            sb1 = new StringBuilder();
+
+            
+            
 
 
         }
-       
+
 
         private void btnStand_click(object sender, RoutedEventArgs e)
         {
-            
+
+
+
+
         }
+
+
+
 
         private void btnDeel_Click(object sender, RoutedEventArgs e)
 
         {
-            
+
             btnHit.IsEnabled = true;
             btnStand.IsEnabled = true;
             btnDeel.IsEnabled = false;
+
+
+
+            Random random = new Random();
+
+            willekeurigeKaart = random.Next(1, 7);
+
+
+
+            sb.AppendLine($"{willekeurigeKaart}");
+            txtSpelSpeler.Text = sb.ToString();
+            scoreSpeler.Text = Convert.ToString(willekeurigeKaart);
+
+
+
+
+
+            willekeurigeKaartBank = random.Next(1, 7);
+
+
+            sb1.AppendLine($"{willekeurigeKaartBank}");
+
+            txtSpelBank.Text = sb1.ToString();
+            ScoreBank.Text = Convert.ToString(willekeurigeKaartBank);
+
             
-            
+
+
+
+
         }
+
+
 
         private void btnHit_Click(object sender, RoutedEventArgs e)
         {
+            //het tweede cijfer + optelling bij het scorebord van Speler
+            Random random = new Random();   
+            int willeKaartSpeler = random.Next(1, 7);
             
+            sb.AppendLine($"{willekeurigeKaart}");
+            txtSpelSpeler.Text = sb.ToString();
+            scoreSpeler.Text = Convert.ToString(willekeurigeKaart + willeKaartSpeler );
+
+            //het tweede cijfer + optelling bij het scorebord van Bank
+            int willeKaartBank = random1.Next(1, 7);
+
+            sb1.AppendLine($"{willekeurigeKaartBank}");
+            txtSpelBank.Text = sb.ToString();  
+            ScoreBank.Text = Convert.ToString( + willeKaartBank);
         }
     }
 }
