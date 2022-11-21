36353d9d509c3b1ffd39df38292836d0d146ca0e
Drozyn84 <Drozyn1984@hotmail.com>
2022-11-21 17:20:14 +0100

    Add project files.

---
+++ Blackjack/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Blackjack
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+
+            btnHit.IsEnabled = false;
+            btnStand.IsEnabled = false;
+         
+
+
+        }
+       
+
+        private void btnStand_click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnDeel_Click(object sender, RoutedEventArgs e)
+
+        {
+            
+            btnHit.IsEnabled = true;
+            btnStand.IsEnabled = true;
+            btnDeel.IsEnabled = false;
+            
+            
+        }
+
+        private void btnHit_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
